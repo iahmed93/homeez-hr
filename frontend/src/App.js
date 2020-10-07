a70bbcd75061f4cd68f9097ce3e1d76e9a024cff
Islam93Ahmed <islam93ahmed@gmail.com>
2020-10-07 17:59:54 +0200

    handle cors error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <NameForm/>
+        </div>
+      </div> 
     </div>
   );
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // alert('A name was submitted: ' + this.state.value);
+    axios.post('http://ec2-15-237-43-220.eu-west-3.compute.amazonaws.com:3000/add-quotation',
+     {"quotation_info": this.state.value}).then(result => {
+       console.log(result);
+     }).catch(error => {
+       console.error(error);
+     })
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label>Quotation Info</label>
+          <input type="text" className="form-control"  value={this.state.value} onChange={this.handleChange}/>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      
+    );
+  }
 }
 
 export default App;
